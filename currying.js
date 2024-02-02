f15b042ccfd30cd1b02ff2f52827fe67a43ff8ef
Abin273 <abinvarghese273@gmail.com>
2024-02-02 09:58:39 +0530

    currying

---
+++ currying.js
@@ -0,0 +1,21 @@
+// Function currying is
+// changing a function having multiple arguments into a sequence of functions with single argument. We can do it using,
+// 1. by using closure
+// 2. by using bind
+
+// without currying
+function one1(a,b){
+    console.log(a*b);
+}
+
+one1(2,4)
+
+// with currying
+function one(a){
+    function two(b){
+        console.log(a*b);
+    }
+    return two
+}
+
+one(2)(4)
